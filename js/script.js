3a0c1ac79cf065e8742d935ea6ac64474e2d69f7
mlietti1 <mcf.lietti@outlook.com>
2022-10-19 20:08:14 +0200

    even odd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-const input = document.querySelector('#palindrome-input');
-const btnPal = document.querySelector('.verifica');
+const inputPal = document.querySelector('#palindrome-input');
+const btnPal = document.querySelector('.check');
 
 btnPal.addEventListener('click', function(){
-  const result = document.getElementById('result-palindrome');
-  printResult(result);
+  const resultPal = document.getElementById('result-palindrome');
+  printResultpal(resultPal);
 })
 
-function printResult(target){
-  target.innerText = checkPalindrome(input.value);
+function printResultpal(target){
+  target.innerText = checkPalindrome(inputPal.value);
 }
 
 function checkPalindrome(string) {
@@ -16,13 +16,43 @@ function checkPalindrome(string) {
 
   for (let i = 0; i < len / 2; i++) {
 
-      if (string[i] !== string[len - 1 - i]) {
-          return 'It is not a palindrome';
-      }
+    if (string[i] !== string[len - 1 - i]) {
+      return 'It is not a palindrome';
+    }
   }
   return 'It is a palindrome';
 }
 
 
 // --------------
+
+
+const inputNum = document.querySelector('#select-number');
+const evenOdd = document.querySelector('#select-evenodd');
+const sum = randomNumber + inputNum.value;
+const btnEvenodd = document.querySelector('.play');
+
+btnEvenodd.addEventListener('click', function(){
+  const resultEvenodd = document.getElementById('result-evenodd');
+  printResultevenodd(resultEvenodd);
+})
+
+function printResultevenodd(target){
+  target.innerText = isEvenOdd;
+}
+
+function isEvenOdd (){
+
+  if (evenOdd.value === 'odd' && (sum % 2)){
+    return 'You won!';
+  }
+
+  return 'Try again...'
+
+}
+
+function getRandomNumber(min, max){
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNumber;
+}
 
