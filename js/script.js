17cbf2a7e53c3990d0c138e13780a45c73ddfe95
mlietti1 <mcf.lietti@outlook.com>
2022-10-19 18:27:24 +0200

    [add] function printResult

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-// ------------ PALINDROMO ---------- //
-
-const input = document.getElementById('palindrome-input').value;
+const string = document.getElementById('palindrome-input').value;
 
 const btn = document.querySelector('.verifica');
 
 btn.addEventListener('click', function(){
   const result = document.getElementById('result-palindrome');
-  result.innerText = checkPalindrome(input);
+  printResult(result);
 })
 
-function checkPalindrome(string) {
-  for (let i = 0; i < string.length / 2; i++)
-
-  if (string[i] !== string[string.length - 1 - i]) {
-    return 'It is not a palindrome.';
-  }
-  return 'It\'s a palindrome.';
+function printResult(target){
+  target.innerText = checkPalindrome(string);
 }
 
+function checkPalindrome(string) {
+
+  const len = string.length;
+
+  for (let i = 0; i < len / 2; i++) {
+
+      if (string[i] !== string[len - 1 - i]) {
+          return 'It is not a palindrome';
+      }
+  }
+  return 'It is a palindrome';
+}
